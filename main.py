b8d26bea3274aa61fb9748be637433fa23d38c0c
TarunTomar122 <tomartarun2001@gmail.com>
2020-03-01 01:36:51 +0530

    first Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+
+width = 600
+height = 600
+
+# initialise pygame
+pygame.init()
+
+# Title and Icon
+pygame.display.set_caption("TicTacToe")
+# icon = pygame.image.load("./assets/sprites/pacman/pacman_l.png")
+# pygame.display.set_icon(icon)
+
+# create the screen
+screen = pygame.display.set_mode((width,height))
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0,0,0))
+
+    pygame.display.update()
